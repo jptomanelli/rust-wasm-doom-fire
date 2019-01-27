25f92cd7daeac2e64ae5a955b1c93ed0b509b682
John Tomanelli II <jptomanelli@gmail.com>
2019-01-27 11:40:15 -0500

    Remove pixel option

--- www/index.js
+++ www/index.js
@@ -1,47 +1,67 @@
 import {
     Fire
 } from "doom-fire";
-import { memory } from "doom-fire/doom_fire_bg";
+import {
+    memory
+} from "doom-fire/doom_fire_bg";
 
-let pixelSize = 5;
-const canvas = document.querySelector('canvas'),
-    ctx = canvas.getContext('2d');
+class DoomAnimation {
 
-canvas.style.width = '100%'
-canvas.style.height = '100%';
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
+    constructor(options = {}) {
+        this.options = Object.assign({
+           canvasSelector: 'canvas'
+        }, options);
 
-const palette = ["#070707", "#1f0707", "#2f0f07", "#470f07", "#571707", "#671f07", "#771f07", "#8f2707", "#9f2f07", "#af3f07", "#bf4707", "#c74707", "#df4f07", "#df5707", "#df5707", "#d75f07", "#d75f07", "#d7670f", "#cf6f0f", "#cf770f", "#cf7f0f", "#cf8717", "#c78717", "#c78f17", "#c7971f", "#bf9f1f", "#bf9f1f", "#bfa727", "#bfa727", "#bfaf2f", "#b7af2f", "#b7b72f", "#b7b737", "#cfcf6f", "#dfdf9f", "#efefc7", "#ffffff"];
-const height = Math.floor(canvas.height / pixelSize);
-const width = Math.floor(canvas.width / pixelSize);
+        this.palette = ["#070707", "#1f0707", "#2f0f07", "#470f07", "#571707", "#671f07", "#771f07", "#8f2707", "#9f2f07", "#af3f07", "#bf4707", "#c74707", "#df4f07", "#df5707", "#df5707", "#d75f07", "#d75f07", "#d7670f", "#cf6f0f", "#cf770f", "#cf7f0f", "#cf8717", "#c78717", "#c78f17", "#c7971f", "#bf9f1f", "#bf9f1f", "#bfa727", "#bfa727", "#bfaf2f", "#b7af2f", "#b7b72f", "#b7b737", "#cfcf6f", "#dfdf9f", "#efefc7", "#ffffff"];
+        this.canvas = document.querySelector(this.options.canvasSelector);
+        this.ctx = canvas.getContext('2d');
 
-const arrLen = height * width;
-
-let fire = Fire.new(height, width, palette.length);
-fire.update_cells();
-const cells = new Uint8Array(memory.buffer, fire.get_cells(), arrLen);
-window.cells = cells;
-function draw() {
-    ctx.clearRect(0, 0, canvas.height, canvas.width);
-    let color, ctr = 0;
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            color = palette[cells[ctr]];
-            ctx.fillStyle = color;
-            ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
-            ctr += 1;
-
-        }
+        this.bind();
+        this.setCanvasDimensions();
+        this.setFireObject();
+        this.draw();
     }
-    fire.update_cells();
-    requestAnimationFrame(draw)
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.height, this.canvas.width);
+        this.fire.update_cells();
+
+        let color, ctr = 0;
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                color = this.palette[this.cells[ctr]];
+                this.ctx.fillStyle = color;
+                this.ctx.fillRect(x * this.pixelSize, y * this.pixelSize, this.pixelSize, this.pixelSize);
+                ctr += 1;
+
+            }
+        }
+        requestAnimationFrame(this.draw.bind(this));
+    }
+
+    bind() {}
+
+    setCanvasDimensions() {
+        this.canvas.style.width = '100%'
+        this.canvas.style.height = '100%';
+        this.canvas.width = canvas.offsetWidth;
+        this.canvas.height = canvas.offsetHeight;
+        this.pixelSize = 8;
+    }
+
+    setFireObject() {
+        this.height = Math.floor(this.canvas.height / this.pixelSize);
+        this.width = Math.floor(this.canvas.width / this.pixelSize);
+
+        const arrLen = this.height * this.width;
+
+        this.fire = Fire.new(this.height, this.width, this.palette.length);
+        this.fire.update_cells();
+
+        this.cells = new Uint8Array(memory.buffer, this.fire.get_cells(), arrLen);
+    }
+
 }
 
-draw();
-
-document.getElementById('pixelSize').addEventListener('change', (e) => {
-    pixelSize = e.target.value;
-    fire = Fire.new(height, width, palette.length);
-    draw();
-});
+const doom_animation = new DoomAnimation();
+window.doom_animation = doom_animation;
